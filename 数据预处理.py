cf1f3418f6fbc0802083852ded5169305b22a1d5
Mensyne <1379022527@qq.com>
2019-02-27 17:32:21 +0800

    评分卡模型的监控

--- 数据预处理.py
+++ 数据预处理.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor
 
-
 plt.rcParams['font.sans-serif'] =['SimHei']  #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] =False   #用来正常显示负号
 

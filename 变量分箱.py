630be44ca5aa36ad1780b6ef203360a6fa8f3836
Mensyne <1379022527@qq.com>
2019-02-27 17:27:52 +0800

    评分卡分值刻度的实现

--- 变量分箱.py
+++ 变量分箱.py
@@ -2,7 +2,7 @@
 import numpy as np
 import  pandas as pd
 import matplotlib.pyplot as plt
-
+import math
 plt.rcParams['font.sans-serif'] =['SimHei']  #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] =False   #用来正常显示负号
 # 变量分箱
